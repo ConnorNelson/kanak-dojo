78880771d7c037f3f808e27afc1845b5aefc08b9
Connor Nelson <Connor@ConnorNelson.com>
2025-01-09 16:52:13 -0700

    Update

--- lecture/lecture/wsgi.py
+++ lecture/lecture/wsgi.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def index():
+    return render_template("index.html", youtube_id="AKTYVWCi6ss")
 
 application = app
